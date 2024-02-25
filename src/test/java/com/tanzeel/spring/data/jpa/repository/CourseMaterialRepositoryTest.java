e95498a28a9ff95325136f5d1774a252696a7c1e
mdtanzeel-ti <md.tanzeel@trilogy.com>
2024-02-25 11:18:31 +0530

    test: Addition of test to insert Course and courseMaterial in db.

---
+++ src/test/java/com/tanzeel/spring/data/jpa/repository/CourseMaterialRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.tanzeel.spring.data.jpa.repository;
+
+import com.tanzeel.spring.data.jpa.entity.Course;
+import com.tanzeel.spring.data.jpa.entity.CourseMaterial;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseMaterialRepositoryTest {
+    @Autowired
+    private CourseMaterialRepository courseMaterialRepository;
+
+    @Test
+    public void saveCourseMaterial() {
+        Course course = Course.builder()
+                .title("Spring-JPA")
+                .credit(5)
+                .build();
+
+        CourseMaterial courseMaterial = CourseMaterial.builder()
+                .url("www.google.com")
+                .course(course)
+                .build();
+
+        courseMaterialRepository.save(courseMaterial);
+    }
+
+}
