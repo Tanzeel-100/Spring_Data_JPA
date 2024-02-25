6696265ea064e6b33173a2b3077fd797aff3ff6e
mdtanzeel-ti <md.tanzeel@trilogy.com>
2024-02-25 11:15:49 +0530

    feature: Implementation of Course and CourseMaterial classes.

---
+++ src/main/java/com/tanzeel/spring/data/jpa/entity/CourseMaterial.java
@@ -0,0 +1,36 @@
+package com.tanzeel.spring.data.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CourseMaterial {
+    @Id
+    @SequenceGenerator(
+            name = "course_material_sequence",
+            sequenceName = "course_material_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_material_sequence"
+    )
+    private Long CourseMaterialId;
+    private String url;
+
+    @OneToOne(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(
+            name = "course_id",
+            referencedColumnName = "courseId"
+    )
+    private Course course;
+}
